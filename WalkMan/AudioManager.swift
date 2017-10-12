d40b23fad84aea914de5caf35d982150d8841133
Unknown <t.kodera.nichibi@gmail.com>
2017-10-12 15:30:28 +0900

    Initial commit

---
+++ WalkMan/AudioManager.swift
@@ -0,0 +1,13 @@
+//
+//  AudioManager.swift
+//  WalkMan
+//
+//  Created by AppCircle on 2017/07/30.
+//  Copyright © 2017年 NichibiAppCircle. All rights reserved.
+//
+
+import UIKit
+
+class AudioManager: NSObject {
+
+}
